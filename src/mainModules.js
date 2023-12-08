0d4f9cff019785f32f89ca547e96520ca2518d22
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2023-12-08 20:05:17 +0100

    modules creating tasks and lists

--- src/mainModules.js
+++ src/mainModules.js
@@ -1,15 +1,44 @@
 function createTask(title, description, date, priority) {
 
     let taskTitle = title;
-    let description = description;
-    let dueDate = date;
-    let priority = priority;
+    let taskDescription = description;
+    let taskDueDate = date;
+    let taskPriority = priority;
+    let taskStatus = "uncompleted";
     
-
-
-
-
     const getTaskTitle = () => taskTitle;
     const setTaskTitle = newTitle => taskTitle = newTitle; 
 
+    const getTaskDescription = () => taskDescription;
+    const setTaskDescription = newDescription => taskDescription = newDescription; 
+
+    const getTaskDueDate = () => taskDueDate;
+    const setTaskDueDate = newDueDate => taskDueDate = newDueDate; 
+
+    const getTaskPriority = () => taskPriority;
+    const setTaskPriority = newPriority => taskPriority = newPriority; 
+
+    const getTaskStatus = () => taskStatus;
+    const changeTaskStatus = () => taskStatus = "completed";
+
+    return { getTaskTitle, setTaskTitle,
+         getTaskDescription, setTaskDescription,
+          getTaskDueDate, setTaskDueDate,
+           getTaskPriority, setTaskPriority,
+            getTaskStatus, changeTaskStatus }
+}
+
+function createTaskList (name, description) {
+    let taskList = [];
+
+    const addTaskToList = () => taskList.push(createTask());
+
+    const getListName = () => name;
+    const setListName = newName => name = newName;
+
+    const getListDescription = () => description;
+    const setListDescription = newDescription => description = newDescription;
+
+
+    return { taskList, addTaskToList, getListName, setListName, getListDescription, setListDescription };
 }
