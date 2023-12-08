258bbea94a0729476e9a5043a010beba7828d9ff
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2023-12-08 19:17:00 +0100

    modules creating tasks and lists

---
+++ src/mainModules.js
@@ -0,0 +1,15 @@
+function createTask(title, description, date, priority) {
+
+    let taskTitle = title;
+    let description = description;
+    let dueDate = date;
+    let priority = priority;
+    
+
+
+
+
+    const getTaskTitle = () => taskTitle;
+    const setTaskTitle = newTitle => taskTitle = newTitle; 
+
+}
